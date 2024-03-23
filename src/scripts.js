37e390fab35f7a353d639a73a05d771a3710ed22
Markus Schnugg <hello@cma5.de>
2024-03-23 16:51:09 +0100

    menu js

--- src/scripts.js
+++ src/scripts.js
@@ -1,14 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Delays = void 0;
-var Delays;
-(function (Delays) {
-    Delays[Delays["SHORT"] = 300] = "SHORT";
-    Delays[Delays["MEDIUM"] = 500] = "MEDIUM";
-    Delays[Delays["LONG"] = 1000] = "LONG";
-})(Delays || (exports.Delays = Delays = {}));
 function toggleVisibility(elementID) {
-    var element = document.getElementById(elementID);
+    var element = document.getElementsByClassName(elementID)[0];
     if (element === null) {
         console.error("Element with ID ".concat(elementID, " not found"));
         return;
@@ -19,4 +10,14 @@ function toggleVisibility(elementID) {
     else {
         element.style.display = "none";
     }
+    //eventlistener for clicking outside of the element
 }
+// event listener for clicking outside of the element
+document.addEventListener('click', function (event) {
+    var element = document.getElementsByClassName('floating-menu')[0];
+    var activeMenuButton = document.getElementsByClassName('active-menu-button')[0];
+    var target = event.target;
+    if (element.style.display === "flex" && !element.contains(target) && !activeMenuButton.contains(target)) {
+        element.style.display = "none";
+    }
+});
--- src/scripts.ts
+++ src/scripts.ts
@@ -1,12 +1,5 @@
-export enum Delays {
-    SHORT = 300,
-    MEDIUM = 500,
-    LONG = 1000,
-}
-
-
 function toggleVisibility(elementID: string) {
-    const element = document.getElementById(elementID)
+    const element:any = document.getElementsByClassName(elementID)[0]
 
     if (element === null) {
         console.error(`Element with ID ${elementID} not found`)
@@ -18,4 +11,18 @@ function toggleVisibility(elementID: string) {
     else {
         element.style.display = "none"
     }
+    //eventlistener for clicking outside of the element
 }
+
+// event listener for clicking outside of the element
+document.addEventListener('click', function(event) {
+    const element:any = document.getElementsByClassName('floating-menu')[0]
+    const activeMenuButton:any = document.getElementsByClassName('active-menu-button')[0]
+    const target = event.target as HTMLElement
+
+    if (element.style.display === "flex" && !element.contains(target) && !activeMenuButton.contains(target)) {
+        element.style.display = "none"
+    }
+})
+
+
